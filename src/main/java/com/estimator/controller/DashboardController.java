228c03196a08e8a3fe0fc9c286415a887b0167b9
diasel2000 <n221296@gmail.com>
2024-08-05 17:00:53 +0300

    Changed dashboard controller

--- src/main/java/com/estimator/controller/DashboardController.java
+++ src/main/java/com/estimator/controller/DashboardController.java
@@ -2,8 +2,8 @@ package com.estimator.controller;
 
 import com.estimator.model.Subscription;
 import com.estimator.model.User;
-import com.estimator.repository.SubscriptionRepository;
-import com.estimator.repository.UserRepository;
+import com.estimator.services.SubscriptionService;
+import com.estimator.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -15,19 +15,41 @@ import java.util.List;
 @Controller
 public class DashboardController {
 
-    private final UserRepository userRepository;
-    private final SubscriptionRepository subscriptionRepository;
+    private final UserService userService;
+    private final SubscriptionService subscriptionService;
 
     @Autowired
-    public DashboardController(UserRepository userRepository, SubscriptionRepository subscriptionRepository) {
-        this.userRepository = userRepository;
-        this.subscriptionRepository = subscriptionRepository;
+    public DashboardController(UserService userService, SubscriptionService subscriptionService) {
+        this.userService = userService;
+        this.subscriptionService = subscriptionService;
     }
 
     @GetMapping("/dashboard")
     public String dashboard(Model model, Authentication authentication) {
-        String username = authentication.getName();
-        User user = userRepository.findByUsername(username);
+        String googleId = authentication.getName();
+
+        User user = userService.findByGoogleID(googleId);
+        List<Subscription> subscriptions = subscriptionService.getAllSubscriptions();
+        for (Subscription subscription:subscriptions){
+            String subscriptionName =subscription.getSubscriptionName();
+            if (subscriptionName.equals("Basic")){
+                model.addAttribute("user", user);
+                return "dashboardB";
+            }
+            if (subscriptionName.equals("Standard")){
+                model.addAttribute("user", user);
+                return "dashboardS";
+            }
+            if (subscriptionName.equals("Ultimate")){
+                model.addAttribute("user", user);
+                return "dashboardU";
+            }
+
+        }
+        //TODO implement role permission check
+
+        //TODO implement templates for difrent subscriptions and roles
+
         model.addAttribute("user", user);
         return "dashboard";
     }
