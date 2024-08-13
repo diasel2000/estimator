8032c39905d19af94661260feae2d9d70cba3bd3
diasel2000 <n221296@gmail.com>
2024-08-13 21:57:53 +0300

    updated private policy

--- src/main/java/com/estimator/services/UserDetailsServiceImpl.java
+++ src/main/java/com/estimator/services/UserDetailsServiceImpl.java
@@ -1,17 +1,22 @@
 package com.estimator.services;
 
 import com.estimator.model.User;
+import com.estimator.model.UserRole;
 import com.estimator.repository.UserRepository;
+import com.estimator.repository.UserRoleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -19,10 +24,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
     private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
 
     @Autowired
-    public UserDetailsServiceImpl(UserRepository userRepository) {
+    public UserDetailsServiceImpl(UserRepository userRepository, UserRoleRepository userRoleRepository) {
         this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
     }
 
     @Override
@@ -34,7 +41,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
             logger.warn("User not found with username: {}", username);
             throw new UsernameNotFoundException("User not found");
         }
-        logger.info("User found with username: {}", username);
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+
+        List<UserRole> userRoles = userRoleRepository.findByUser(user);
+        List<GrantedAuthority> authorities = userRoles.stream()
+                .map(userRole -> new SimpleGrantedAuthority(userRole.getRole().getRoleName()))
+                .collect(Collectors.toList());
+
+        logger.info("User found with username: {} and roles: {}", username, authorities);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 }
