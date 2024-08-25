130f386bc6aed568a82a210b1ede705f8dfabe8e
diasel2000 <n221296@gmail.com>
2024-08-25 15:33:47 +0300

    Added fixes for redirections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
   standalone: true,
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, CommonModule, RouterModule]
 })
 export class AppComponent {
-  constructor(private router: Router) {}
+  currentUrl: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const url = this.router.url;
+      this.currentUrl = url;
+      const fragment = this.router.parseUrl(url).fragment;
+      if (this.isHomePage() && fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  }
 
   navigateToLogin() {
-    console.log("go")
-    this.router.navigate(['login']);
+    this.router.navigate(['/login']);
+  }
+
+  isHomePage(): boolean {
+    // Check if the current URL is either the root '/' or the root with a fragment
+    return this.currentUrl === '/' || this.currentUrl.startsWith('/') && this.router.parseUrl(this.currentUrl).fragment !== undefined;
+  }
+
+  shouldDisplayHomePage(): boolean {
+    // Display home page content if the current route is home or contains a fragment
+    return this.isHomePage() && (this.currentUrl === '/' || this.currentUrl.includes('#'));
   }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class LoginComponent {
   email: string = '';
