bb51f254f976b3acdab4727c5bb3b98485212b52
diasel2000 <n221296@gmail.com>
2024-09-17 16:03:12 +0300

    modal more updates

--- src/app/components/more/more.component.ts
+++ src/app/components/more/more.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-more',
   standalone: true,
-  imports: [],
+  imports: [CommonModule
+  ],
   templateUrl: './more.component.html',
   styleUrl: './more.component.sass'
 })
 export class MoreComponent {
+  isOpen = false;
 
+  toggleModal() {
+    this.isOpen = !this.isOpen;
+  }
 }
