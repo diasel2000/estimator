3c181784ae244461805b977749aef993e197867b
diasel2000 <n221296@gmail.com>
2024-09-19 16:48:12 +0300

    updated main styles and added mock developers component

---
+++ src/app/components/developer-dialog/developer-dialog.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { } from '@angular/material';
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+
+interface CustomField {
+  title: string;
+  description: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-developer-dialog',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './developer-dialog.component.html',
+  styleUrls: ['./developer-dialog.component.sass']
+})
+export class DeveloperDialogComponent {
+  developer = {
+    name: '',
+    position: '',
+    customFields: [] as CustomField[]
+  };
+
+  constructor(public dialogRef: MatDialogRef<DeveloperDialogComponent>) {}
+
+  addCustomField(): void {
+    this.developer.customFields.push({ title: '', description: '', link: '' });
+  }
+
+  removeCustomField(index: number): void {
+    this.developer.customFields.splice(index, 1);
+  }
+
+  addDeveloper(): void {
+    this.dialogRef.close(this.developer);
+  }
+}
--- src/app/components/developers/developers.component.ts
+++ src/app/components/developers/developers.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { } from '@angular/material';
+import {DeveloperDialogComponent} from "../developer-dialog/developer-dialog.component";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-developers',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule, CommonModule, FormsModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatToolbarModule
+  ],
   templateUrl: './developers.component.html',
-  styleUrl: './developers.component.sass'
+  styleUrls: ['./developers.component.sass']
 })
 export class DevelopersComponent {
+  developers: any[] = [];
+  currentPage = 1;
+  pageSize = 8;
 
+  constructor(private dialog: MatDialog) {}
+
+  openAddDeveloperDialog(): void {
+    const dialogRef = this.dialog.open(DeveloperDialogComponent, {
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.developers.push(result);
+      }
+    });
+  }
+
+  get paginatedDevelopers() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    return this.developers.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
+  }
 }
